31a61d5166aaac812bbcf86adb3fb05e4b18b711
김송은 <158058195+sonngneee@users.noreply.github.com>
2024-08-22 11:35:24 +0900

    Title: 싸움땅, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240822/싸움땅/battle-ground.cpp
+++ 240822/싸움땅/battle-ground.cpp
@@ -12,7 +12,7 @@ int dx[4] = { 0, 1, 0, -1 };
 
 vector<int> map_gun[21][21]; // 총공격력
 int map_player[21][21]; // 플레이어 번호 맵 >> 플레이어 움직일 때마다 업데이트 
-int score_player[21]; // // 플레이어에 따른 포인트 arr : 플레이어 번호 1부터 시작
+int score_player[31]; // // 플레이어에 따른 포인트 arr : 플레이어 번호 1부터 시작
 
 struct player_info {
 	int num, y, x, d, s, isGun, gunS; // num, y, x, 방향, 총기여부, 총 공격력
@@ -63,13 +63,8 @@ void change_gun(player_info* winP, int ny, int nx) {
 			int temp = map_gun[ny][nx].front();
 			map_gun[ny][nx][0] = winP->gunS;
 			winP->gunS = temp;		
-		}
+		}  
 	}
-	// gun sort
-	// 제일 공격력이 센 거
-	// 승리한 칸에 떨어져 있는 총들과 원래 들고 있던 총 중 가장 공격력이 높은 총을 획득
-	// 나머지 총들은 해당 격자에 내려 놓습니다.
-
 }
 
 void fight(player_info* nowP, int ny, int nx) {
@@ -104,19 +99,8 @@ void fight(player_info* nowP, int ny, int nx) {
 		}
 	}
 
-	//	2-1-1. 이긴 플레이어는 각 플레이어의 초기 능력치와 가지고 있는 총의 공격력의 합의 차이만큼을 포인트로 획득하게 됩니다.
-	//			승리한 칸에 떨어져 있는 총들과 원래 들고 있던 총 중 가장 공격력이 높은 총을 획득	
-	// `		나머지 총들은 해당 격자에 내려 놓습니다.
-
 	score_player[winP->num] += abs(nowP_score - figP_score);
 
-	//	2-1-2. 진 플레이어는 본인이 가지고 있는 총을 해당 격자에 내려놓고
-	//			해당 플레이어가 원래 가지고 있던 방향대로 한 칸 이동
-	//			다른 플레이어가 있거나 격자 범위 밖인 경우에는 오른쪽으로 90도씩 회전하여 빈 칸이 보이는 순간 이동
-	//				만약 해당 칸에 총이 있다면, 해당 플레이어는 가장 공격력이 높은 총을 획득하고 나머지 총들은 해당 격자에 내려 놓습니다.
-	//		
-
-		// 맵 업데이트
 	map_player[winP->y][winP->x] = 0;
 	map_player[loseP->y][loseP->x] = 0;
 
@@ -156,7 +140,7 @@ void fight(player_info* nowP, int ny, int nx) {
 	}
 
 	if (!map_gun[winP->y][winP->x].empty()) {
-		change_gun(winP, ny, nx);
+ 		change_gun(winP, ny, nx);
 	}
 
 }
@@ -170,9 +154,6 @@ void move(int np) {
 	player_info* nowP = &players[np];
 	ny = nowP->y + dy[nowP->d];
 	nx = nowP->x + dx[nowP->d];
-
-	//  1. 첫 번째 플레이어부터 순차적으로 본인이 향하고 있는 방향대로 한 칸만큼 이동 /
-	//		만약 해당 방향으로 나갈 때 격자를 벗어나는 경우에는 정반대 방향으로 방향을 바꾸어서 1만큼 이동합니다.
 
 	if (ny < 0 || nx < 0 || ny >= n || nx >= n) {
 		nowP->d = (nowP->d + 2) % 4; // 방향 바꾸기
@@ -188,7 +169,7 @@ void move(int np) {
 	}
 	else if (! map_gun[ny][nx].empty()) {
 		// 총이 있는 경우
-		change_gun(nowP, ny, nx);
+		change_gun(nowP, ny, nx); 
 	}
 
 	// 맵 업데이트
@@ -212,7 +193,7 @@ void process() {
 
 int main() {
 
-	// freopen("sample_input.txt", "r", stdin);
+	//freopen("sample_input.txt", "r", stdin);
 
 	input();
 	process();
