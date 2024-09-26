d7b8b249e295e7642beda6eab769d93d022fe785
김송은 <158058195+sonngneee@users.noreply.github.com>
2024-09-26 22:38:32 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240926/트로미노/tromino.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int map[201][201];
+
+void Input() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+		}
+	}
+}
+
+int maxval = 0;
+void check1(int r, int c) {
+	
+	if (r + 1 >= n || c + 1 >= n) return;
+
+	int sqr = map[r][c] + map[r + 1][c] + map[r][c + 1] + map[r + 1][c + 1];
+	for (int i = r; i < r + 2; i++) {
+		for (int j = c; j < c + 2; j++) {
+			maxval = max(maxval, sqr - map[i][j]);
+		}
+	}
+}
+
+void check2(int r, int c) {
+	
+	if (r + 2 >= n) return;
+	int sqr = 0;
+
+	for (int row = r; row < r + 3; row++) {
+		sqr += map[row][c];
+	}
+	maxval = max(maxval, sqr);
+}
+
+void check3(int r, int c) {
+
+	if (c + 2 >= n) return;
+	int sqr = 0;
+
+	for (int col = c; col < c + 3; col++) {
+		sqr += map[r][col];
+	}
+	maxval = max(maxval, sqr);
+}
+
+void Process() {
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			check1(i, j);
+			check2(i, j);
+			check3(i, j);
+		}
+	}
+	cout << maxval;
+}
+
+int main() {
+
+	Input();
+	Process();
+
+	return 0;
+}
