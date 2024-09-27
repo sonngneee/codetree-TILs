f43fa287d6e7c6817a96a2eb1a949ddb804cd51e
김송은 <158058195+sonngneee@users.noreply.github.com>
2024-09-27 09:20:47 +0900

    Title: 양수 직사각형의 최대 크기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240927/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int n, m;
+int map[21][21];
+
+void Input() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+		}
+	}
+}
+
+struct pos {
+	int y, x;
+};
+
+int dy[3] = { 0, 1, 1 };
+int dx[3] = { 1, 0, 1 };
+
+int Calculation(pos st, pos ed) {
+	int cnt =0;
+
+	for (int i = st.y; i <= ed.y; i++) {
+		for (int j = st.x; j <= ed.x; j++) {
+			if (map[i][j] < 0) return 0;
+			cnt++;
+		}
+	}
+	return cnt;
+}
+
+int ans;
+
+void Process() {
+	pos st, ed;
+	int maxval = 0;
+	for (int sy = 0; sy < n; sy++) {
+		for (int sx = 0; sx < m; sx++) {
+			st = { sy, sx };
+			for (int ey = st.y; ey < n;  ey++) {
+				for (int ex = st.x; ex < m; ex++) {
+					ed = { ey, ex };
+
+					ans = max(ans, Calculation(st, ed));
+				}
+			}
+		}
+	}
+}
+
+int main() {
+
+	// freopen("sample_input.txt", "r", stdin);
+	Input();
+	Process();
+	cout << ans;
+	return 0;
+}
