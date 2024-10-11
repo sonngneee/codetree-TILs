d4d6ab05bfceb491cfcff97df2b01e648153573f
김송은 <158058195+sonngneee@users.noreply.github.com>
2024-10-11 15:36:45 +0900

    Title: 1차원 폭발 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241011/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 241011/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -1,93 +1,95 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARININGS
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <queue>
 
 using namespace std;
 
+int map[101];
 int N, M;
-vector<int> bomb;
-int isEnd = 0;
+
+int isTrue = 0;
+
+int tempmap[101];
+int bsize;
 
 void Input() {
 	cin >> N >> M;
-	int a;
 	for (int i = 0; i < N; i++) {
-		cin >> a;
-		bomb.push_back(a);
+		cin >> map[i];
 	}
 }
 
-void Bomb() {
-	int bsize = bomb.size();
-	vector<int> b;
-	vector<int> temp;
-	int now = bomb[0];
+void Del() {
+	vector<int> del;
+	vector<int> temp;	// 임시저장용
+	int now = map[0];
+	temp.push_back(0);
 	int cnt = 1;
 
-	temp.push_back(0);
 	for (int i = 1; i < bsize; i++) {
-		int next = bomb[i];
+		int next = map[i];
+
 		if (now == next) {
 			cnt++;
 			temp.push_back(i);
 			now = next;
-			if ((i == bsize - 1) && temp.size() >= M) {
-				for (int j = 0; j < cnt; j++) {
-					b.push_back(temp[j]);
+			if (i == bsize - 1) {
+				if (cnt >= M) {
+					for (int p = 0; p < temp.size(); p++) {
+						del.push_back(temp[p]);
+					}
 				}
 			}
 		}
 		else {
 			if (cnt >= M) {
-				for (int j = 0; j < cnt; j++) {
-					b.push_back(temp[j]);
+				for (int p = 0; p < temp.size(); p++) {
+					del.push_back(temp[p]);
 				}
 			}
+			cnt = 1;
 			temp.clear();
 			temp.push_back(i);
-			cnt = 1;
 			now = next;
 		}
 	}
-	
-	bsize = b.size();
-	if (bsize == 0) {
-		isEnd = 1;
+
+	if (del.size() == 0) {
+		isTrue = 1;
 		return;
 	}
-	for (auto d : b) {
-		bomb[d] = 0;
-	}
-	int bombsize = bomb.size();
-	for (int i = bombsize - 1; i >=0; i--) {
-		if (bomb[i] == 0) {
-			bomb.erase(bomb.begin() + i);
-		}
-	}
-	if(bomb.size() == 0) isEnd = 1;
 
+	for (auto p : del) {
+		map[p] = 0;
+	}
+
+	cnt = 0;
+	
+	for (int i = 0; i < bsize; i++) {
+		if (map[i] == 0) continue;
+		tempmap[cnt] = map[i];
+		cnt++;
+	}
+	memcpy(map, tempmap, sizeof(map));
+	bsize = cnt;
 }
 
 void Process() {
-	while (! isEnd) {
-		Bomb();
+	bsize = N;
+	while (!isTrue) {
+		memset(tempmap, 0, sizeof(tempmap));
+		Del();
 	}
-
+	cout << bsize<<'\n';
+	for (int i = 0; i < bsize; i++) {
+		cout << map[i] << '\n';
+	}
 }
 int main() {
-
-	//freopen("input.txt", "r", stdin);
-	Input();
 	
+	Input();
 	Process();
-
-	int ccnt = bomb.size();
-	cout << ccnt<<'\n';
-	for (int i = 0; i < ccnt; i++) {
-		cout << bomb[i] << '\n';
-	}
-
 	
 	return 0;
 }
